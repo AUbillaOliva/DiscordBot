564147656ae8aa08d6e8f626068be3c43c08c8d6
Alvaro Ubilla Oliva <afubillaoliva@gmail.com>
2020-05-21 18:06:48 -0400

    Added index.js; ping, weather and covid functionality implemented

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client_key = require("./config/config.js");
+
+const getWeather = require("./modules/weather/weather.js");
+const ping = require('./modules/ping/ping.js');
+const covid = require('./modules/covid/covid.js');
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("BRIGIDOOO", {
+    type: "STREAMING",
+    url: "https://www.twitch.tv/argrinus",
+  });
+});
+
+client.on("message", async (msg) => {
+  let content = msg.content;
+  if (content === "ping") {
+      ping(msg);
+  } else if (content.startsWith("tiempo")) {
+    getWeather(msg);
+  } else if (content.startsWith("covid")){
+      covid(msg);
+  }
+});
+
+client.login(client_key);
