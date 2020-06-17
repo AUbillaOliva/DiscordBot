95daae6d73c37054e768065eca0ccf3a4a266af9
AUbillaOliva <afubillaoliva@gmail.com>
2020-06-17 01:28:28 -0400

    added news functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client_key = require("./config/config.js");
 const getWeather = require("./modules/weather/weather.js");
 const ping = require('./modules/ping/ping.js');
 const covid = require('./modules/covid/covid.js');
+const news = require('./modules/news/news.js');
 
 const client = new Discord.Client();
 
@@ -17,12 +18,14 @@ client.on("ready", () => {
 
 client.on("message", async (msg) => {
   let content = msg.content;
-  if (content === "ping") {
+  if (content.startsWith('ping')) {
       ping(msg);
-  } else if (content.startsWith("tiempo")) {
+  } else if (content.startsWith('tiempo')) {
     getWeather(msg);
-  } else if (content.startsWith("covid")){
-      covid(msg);
+  } else if (content.startsWith('covid')){
+    covid(msg);
+  } else if (content.startsWith('news')){
+    news(msg); 
   }
 });
 
