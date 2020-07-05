dd7b42e55aae4fe5dc6b106f41cf46d31697f686
AUbillaOliva <afubillaoliva@gmail.com>
2020-07-05 09:07:27 -0400

    :wrench: ignoring config files

--- tools/covid/config/config.js
+++
@@ -1,8 +0,0 @@
-const config = require('config');
-const API_KEY = config.get('covid.apiKey');
-const API_HOST = config.get('covid.apiHost');
-const API_URL = config.get('covid.apiUrl');
-
-exports.apiHost = API_HOST;
-exports.apiKey = API_KEY;
-exports.apiUrl = API_URL;
--- tools/news/config/config.js
+++
@@ -1,6 +0,0 @@
-const config = require('config');
-const API_KEY = config.get('news.apiKey');
-const API_URL = config.get('news.apiUrl');
-
-exports.apiKey = API_KEY;
-exports.apiUrl = API_URL;
