566819b1f8350e05f87c55997fd07dc76ce55d1a
AUbillaOliva <afubillaoliva@gmail.com>
2021-02-07 06:48:29 -0300

    :wrench: Added bot status

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("|help");
 });
 
 client.on("message", async (msg) => {
